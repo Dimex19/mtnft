c7bdaf0c3821ed4ec9ab18e4421acb4632afa930
Dimex19 <azeezabolaji06gmail.com>
2025-01-26 22:21:42 +0100

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import Logo from './assets/images/logo.png'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Marquee from './components/Marquee'
+
+import { BrowserRouter, Routes, Route, } from 'react-router-dom'
 import Home from './pages/Home'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
